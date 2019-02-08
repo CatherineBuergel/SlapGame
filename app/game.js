a1ff9032c7ff80432537059866a671d424fd5a90
Catherine Buergel <catherinebuergel@gmail.com>
2019-02-08 14:31:53 -0700

    hits and name buttons

--- app/game.js
+++ app/game.js
@@ -1,12 +1,29 @@
 var health = 100;
+var name = 'Jerry';
+var hits = 0;
 
 function slap() {
   health -= 1;
+  hits += 1;
+  update();
+}
+
+function punch() {
+  health -= 5;
+  hits += 1;
+  update();
+}
+
+function kick() {
+  health -= 10;
+  hits += 1;
   update();
 }
 
 function update() {
-  document.getElementById('health').innerText = health.toString()
+  document.getElementById('health').innerText = health.toString();
+  document.getElementById('hits').innerText = hits.toString();
+  document.getElementById('name').innerText = name;
 }
 
 update()
