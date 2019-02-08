14dcccc1c79d55c0fc1d20f4990c6af4c61f01fa
Catherine Buergel <catherinebuergel@gmail.com>
2019-02-08 14:23:22 -0700

    slap button

--- app/game.js
+++ app/game.js
@@ -1,1 +1,13 @@
-console.log('linked')
+var health = 100;
+
+function slap() {
+  health -= 1;
+  update();
+}
+
+function update() {
+  document.getElementById('health').innerText = health.toString()
+}
+
+update()
+
