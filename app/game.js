5e457034e7b7f5657deeb19abb9ddfd426d1b18d
Catherine Buergel <catherinebuergel@gmail.com>
2019-02-08 15:51:10 -0700

    meat and potatoes

--- app/game.js
+++ app/game.js
@@ -1,29 +1,53 @@
-var health = 100;
-var name = 'Jerry';
-var hits = 0;
 
-function slap() {
-  health -= 1;
-  hits += 1;
+var player = {
+  health: 100,
+  name: 'Jerry',
+  hits: 0,
+  items: []
+}
+
+var items = {
+  shame: {
+    name: 'Shame',
+    modifier: 2,
+    description: 'shame'
+  },
+  mock: {
+    name: 'mock',
+    modifier: 5,
+    description: 'like shame but meaner'
+  },
+  sympathy: {
+    name: 'sympathy',
+    modifier: -5,
+    description: 'Jerry\'s only defense'
+  }
+}
+
+function damage(hitNum) {
+  player.health -= hitNum + addMods();
+  player.hits += 1;
   update();
 }
 
-function punch() {
-  health -= 5;
-  hits += 1;
-  update();
+function giveItem(item) {
+  player.items.push(items[item].modifier)
 }
 
-function kick() {
-  health -= 10;
-  hits += 1;
-  update();
+function addMods() {
+  let currentMods = 0;
+  for (let i = 0; i < player.items.length; i++) {
+    currentMods += player.items[i];
+  }
+  return currentMods;
 }
+
+
 
 function update() {
-  document.getElementById('health').innerText = health.toString();
-  document.getElementById('hits').innerText = hits.toString();
-  document.getElementById('name').innerText = name;
+  document.getElementById('health').innerText = player.health.toString();
+  document.getElementById('hits').innerText = player.hits.toString();
+  document.getElementById('name').innerText = player.name;
 }
 
 update()
