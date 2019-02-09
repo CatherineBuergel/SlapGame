27d672a9752b7d022e1e000991c179fd0b3c3e44
Catherine Buergel <catherinebuergel@gmail.com>
2019-02-09 14:25:37 -0700

    ch-ch-changes

--- app/game.js
+++ app/game.js
@@ -1,9 +1,15 @@
 
 var player = {
   health: 100,
-  name: 'Jerry',
+  name: 'Morty',
   hits: 0,
   items: []
+}
+
+var opponent = {
+  health: 100,
+  name: 'Greaser Morty',
+  hits: 0,
 }
 
 var items = {
@@ -24,14 +30,27 @@ var items = {
   }
 }
 
+function opponentAttack() {
+  setTimeout(function () {
+    player.health -= Math.floor(Math.random() * 10);
+    opponent.hits += 1;
+  }, 1000)
+}
+
 function damage(hitNum) {
-  player.health -= hitNum + addMods();
+  opponent.health -= hitNum + addMods();
   player.hits += 1;
+  opponentAttack();
   update();
 }
 
 function giveItem(item) {
-  player.items.push(items[item].modifier)
+  if (player.items.length == 0) {
+    player.items.push(items[item].modifier)
+  } else {
+    player.items.pop();
+    player.items.push(items[item].modifier)
+  }
 }
 
 function addMods() {
@@ -44,10 +63,21 @@ function addMods() {
 
 
 
+
+
+
+
+
 function update() {
-  document.getElementById('health').innerText = player.health.toString();
+  document.getElementById('player-health').innerText = player.health.toString();
+  document.getElementById('player-health').setAttribute('style', `width: ${player.health.toString()}%`)
+  document.getElementById('opponent-health').innerText = opponent.health.toString();
+  document.getElementById('opponent-health').setAttribute('style', `width: ${opponent.health.toString()}%`)
+  document.getElementById('player-name').innerText = player.name;
+  document.getElementById('opponent-name').innerText = opponent.name;
   document.getElementById('hits').innerText = player.hits.toString();
-  document.getElementById('name').innerText = player.name;
+  document.getElementById('opponent-hits').innerText = opponent.hits.toString();
+
 }
 
 update()
